776ee33c2c3e61995b19102e7c5750f85d03f409
Bjarte Stien Karlsen <bjarte@bjartek.org>
2023-10-08 20:42:29 +0200

    fixed lazyvim

--- .config/lazyvim/lua/config/lazy.lua
+++ .config/lazyvim/lua/config/lazy.lua
@@ -10,9 +10,12 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.test.core" },
     --    { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "plugins" },
   },
